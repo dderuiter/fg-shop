5375425795b867b2e522da548f7a2c4c93abee86
dnd <davisderuiter@gmail.com>
2020-08-23 00:14:09 -0400

    Add item shop images for 2020-08-23

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,22 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
-import { FooterComponent } from './components/footer/footer.component';
-import { ShopComponent } from './components/shop/shop.component';
-import { FeaturedItemComponent } from './components/shop/featured-item/featured-item.component';
-import { RegularItemComponent } from './components/shop/regular-item/regular-item.component';
-import { CountdownTimerComponent } from './components/countdown-timer/countdown-timer.component';
 import { HeaderComponent } from './components/header/header.component';
+import { ShopComponent } from './components/shop/shop.component';
+import { ShopItemComponent } from './components/shop/shop-item/shop-item.component';
+import { CountdownTimerComponent } from './components/countdown-timer/countdown-timer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent,
+    HeaderComponent,
     ShopComponent,
-    FeaturedItemComponent,
-    RegularItemComponent,
-    CountdownTimerComponent,
-    HeaderComponent
+    ShopItemComponent,
+    CountdownTimerComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/shop/shop.component.ts
+++ src/app/components/shop/shop.component.ts
@@ -6,6 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shop.component.scss']
 })
 export class ShopComponent implements OnInit {
+  featuredItemImages: string[] = [
+    "featured-item-1.jpg",
+    "featured-item-2.jpg",
+    "featured-item-3.jpg"
+  ];
+
+  regularItemImages: string[] = [
+    "regular-item-1.jpg",
+    "regular-item-2.jpg",
+    "regular-item-3.jpg",
+    "regular-item-4.jpg",
+    "regular-item-5.jpg",
+    "regular-item-6.jpg"
+  ]
 
   constructor() { }
 
